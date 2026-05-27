1656847af4ba629b9835ad8332e02568a577a5ab
PearTNhat <letuannhat105@gmail.com>
2026-05-27 08:58:45 +0000

    feat: implement persistent Go-binary spam stress tests and update RPC contract addresses

--- test-simple/test-rpc/spam_search.sh
+++ test-simple/test-rpc/spam_search.sh
@@ -2,26 +2,26 @@
 echo "⚡ Đang build Go binary để xả bão tốc độ cao..."
 go build -o spam_bin main.go
 
-echo "🚀 Bắt đầu spam GetData_View vô tận (Ctrl+C để thoát)..."
+echo "🚀 Bắt đầu spam QuerySearch_View vô tận (Ctrl+C để thoát)..."
 trap "echo '🛑 Đã dừng spam!'; exit 0" SIGINT SIGTERM
 
 round=1
 while true; do
   echo "🌀 Spam vòng thứ $round..."
   
-  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-t1.json &
+  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-search-t1.json &
   PID1=$!
   
-  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-t2.json &
+  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-search-t2.json &
   PID2=$!
   
-  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-t3.json &
+  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-search-t3.json &
   PID3=$!
   
-  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-t4.json &
+  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-search-t4.json &
   PID4=$!
   
-  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-t5.json &
+  ./spam_bin -config=./config-local.json -data=./test_read_wire_xapian/spam/data-xapian-search-t5.json &
   PID5=$!
   
   wait $PID1 || { echo "❌ Lỗi ở luồng 1! Thoát..."; exit 1; }
